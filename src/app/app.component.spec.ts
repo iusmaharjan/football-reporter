ebac984e46d05baedd3ff4113ee668035cdc8dd6
Ayush Maharjan <ius.maharjan@gmail.com>
2020-03-18 13:47:17 -0400

    Add logo/font and create navigation bar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'football-reporter'`, () => {
+  it(`should have as title 'Football Reporter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('football-reporter');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('football-reporter');
+    expect(app.title).toEqual('Football Reporter');
   });
 });
